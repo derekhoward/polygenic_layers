555b3d672312a5d974d26f73efe2740da9d12bfc
Derek Howard <derekhoward@gmail.com>
2018-09-20 17:05:05 -0400

    cleaned up old files

--- polygenic_layers/config.py
+++
@@ -1,8 +0,0 @@
-import platform
-import os
-from pathlib import Path
-
-if platform.node() == 'RES-C02TQ1V6G8WL.local':
-	RAW_DATA = Path("/Users/derek_howard/projects/HBAsets/data/raw")
-	DATA_DIR = Path("/Users/derek_howard/projects/polygenic_layers/data")
-	CORES = 6
