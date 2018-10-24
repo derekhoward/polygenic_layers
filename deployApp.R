3cfe4a6b365bb012de0c1312f82645b7799482da
Leon French <leonfrench@gmail.com>
2018-10-24 09:49:48 -0400

    Disable update prompt

--- deployApp.R
+++ deployApp.R
@@ -1,3 +1,3 @@
 library(rsconnect)
 #since we have raw data that's big, only deploy whats needed
-deployApp(appFileManifest = "appFileManifestForDeployment.txt")
+deployApp(appFileManifest = "appFileManifestForDeployment.txt", forceUpdate=T)
